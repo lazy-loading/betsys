5261921c0022fb1ed0577e6ad2d76d308f6a23cd
Alexander Ivanov <sashko99@hotmail.com>
2018-06-24 13:56:31 +0300

    Imports Views

--- BettingSystem/BettingSystem.Web/Areas/Admin/Controllers/ImportsController.cs
+++ BettingSystem/BettingSystem.Web/Areas/Admin/Controllers/ImportsController.cs
@@ -1,15 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using BettingSystem.Imports;
+using BettingSystem.Models;
+using BettingSystem.Services;
 using BettingSystem.Web.Areas.Admin.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BettingSystem.Web.Areas.Admin.Controllers
 {
     [Area(nameof(BettingSystem.Web.Areas.Admin))]
+    [AllowAnonymous]
     public class ImportsController : Controller
     {
+        readonly IEventService mEventService;
+
+        public ImportsController(IEventService eventService)
+        {
+            mEventService = eventService;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -17,7 +30,26 @@ namespace BettingSystem.Web.Areas.Admin.Controllers
 
         public IActionResult ImportUpcomingEvents(UpcomingEventsImportViewModel vm)
         {
-            throw new NotImplementedException();
+            using (var textReader = new StreamReader(vm.XmlFile.OpenReadStream()))
+            {
+                string xml = textReader.ReadToEnd();
+                IEnumerable<SportEvent> events = XmlConverter.FromXml(xml);
+                mEventService.Create(events);
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult ImportUpdateEvents(UpdateEventsImportViewModel vm)
+        {
+            using (var textReader = new StreamReader(vm.XmlFile.OpenReadStream()))
+            {
+                string xml = textReader.ReadToEnd();
+                IEnumerable<SportEvent> events = XmlConverter.FromXml(xml);
+                mEventService.Update(events);
+            }
+
+            return RedirectToAction(nameof(Index));
         }
     }
 }
