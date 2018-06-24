ae1fd5028e358a927b938598ad3b38584b50067a
Albert Stefanov <aastefanov@outlook.com>
2018-06-24 13:31:32 +0300

    Register and login forms

--- BettingSystem/BettingSystem.Web/Startup.cs
+++ BettingSystem/BettingSystem.Web/Startup.cs
@@ -78,8 +78,8 @@ namespace BettingSystem.Web
             app.UseHttpsRedirection();
             app.UseMvc(routes =>
             {
-                routes.MapRoute(name: "default", template: "{controller=Home}/{action=Index}/{id?}");
                 routes.MapRoute(name: "areas", template: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+                routes.MapRoute(name: "default", template: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
