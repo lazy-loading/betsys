7c31af11951a8b832ebfdd9cf91a355afb8f5fdf
Alexander Ivanov <sashko99@hotmail.com>
2018-06-24 13:41:41 +0300

    xml submit controllers

---
+++ BettingSystem/BettingSystem.Web/Areas/Admin/Controllers/ImportsController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BettingSystem.Web.Areas.Admin.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BettingSystem.Web.Areas.Admin.Controllers
+{
+    [Area(nameof(BettingSystem.Web.Areas.Admin))]
+    public class ImportsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ImportUpcomingEvents(UpcomingEventsImportViewModel vm)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
