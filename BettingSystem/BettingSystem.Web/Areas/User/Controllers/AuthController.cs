9351de2a948821276f68d0a4b8219fea9d351e3b
Albert Stefanov <aastefanov@outlook.com>
2018-06-24 14:19:51 +0300

    Login partials

--- BettingSystem/BettingSystem.Web/Areas/User/Controllers/AuthController.cs
+++ BettingSystem/BettingSystem.Web/Areas/User/Controllers/AuthController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using System.Threading.Tasks;
+using BettingSystem.Web.Areas.User.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BettingSystem.Web.Areas.User.Controllers
@@ -6,27 +9,58 @@ namespace BettingSystem.Web.Areas.User.Controllers
     [Area("User")]
     public class AuthController : Controller
     {
+        private readonly SignInManager<BettingSystem.Models.User> _signInManager;
+        private readonly UserManager<BettingSystem.Models.User> _userManager;
+
+        public AuthController(SignInManager<BettingSystem.Models.User> signInManager,
+            UserManager<BettingSystem.Models.User> userManager)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+
+
         [AllowAnonymous]
         public IActionResult Index() => View();
 
         [HttpPost]
         [AllowAnonymous]
-        public IActionResult Login()
+        public async Task<IActionResult> Login([FromBody] UserLoginViewModel model, string returnUrl = "/")
         {
-            return Ok();
+            if (!ModelState.IsValid) return BadRequest();
+            
+            var result = await _signInManager.PasswordSignInAsync(model.EmailAddress, model.Password, false, false);
+
+            if (result.Succeeded) return Redirect(returnUrl);
+
+            return View(nameof(Index), model);
         }
 
         [HttpPost]
         [AllowAnonymous]
-        public IActionResult Register()
+        public async Task<IActionResult> Register([FromBody] UserRegistrationViewModel model, string returnUrl = "/")
         {
-            return Ok();
+            if (!ModelState.IsValid) return BadRequest();
+
+            var user = new BettingSystem.Models.User
+            {
+                UserName = model.EmailAddress,
+                Email = model.EmailAddress
+            };
+
+            var result = await _userManager.CreateAsync(user, model.Password);
+
+            if (result.Succeeded) return Redirect(returnUrl);
+
+            return View(nameof(Index), model);
         }
 
         [HttpPost]
-        public IActionResult Logout()
+        public async Task<IActionResult> Logout()
         {
-            return Ok();
+            await _signInManager.SignOutAsync();
+
+            return RedirectToAction(nameof(Index));
         }
     }
 }
